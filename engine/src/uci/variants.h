015a83764944d99aea72c72e8ce2bce1e53e8dfb
Jannis Blüml <me@jblue.ml>
2022-05-25 10:26:09 +0200

    add RBC

--- engine/src/uci/variants.h
+++ engine/src/uci/variants.h
@@ -62,6 +62,7 @@ const static vector<string> availableVariants = {
 #ifdef MODE_OPEN_SPIEL
     "hex",
     "darkhex",
+    "rbc",
 #endif
 };
 
@@ -71,76 +72,76 @@ const int SUBVARIANT_NB = 20; // Thats high quality code
 #endif
 
 const static string StartFENs[SUBVARIANT_NB] = {
-    
-    #ifdef MODE_OPEN_SPIEL
-    ". . . . . . . . . . .  . . . . . . . . . . .   . . . . . . . . . . .    . . . . . . . . . . .     . . . . . . . . . . .      . . . . . . . . . . .       . . . . . . . . . . .        . . . . . . . . . . .         . . . . . . . . . . .          . . . . . . . . . . .           . . . . . . . . . . .",    
-    #endif
-        
-    #ifdef ANTI
+
+#ifdef MODE_OPEN_SPIEL
+    ". . . . . . . . . . .  . . . . . . . . . . .   . . . . . . . . . . .    . . . . . . . . . . .     . . . . . . . . . . .      . . . . . . . . . . .       . . . . . . . . . . .        . . . . . . . . . . .         . . . . . . . . . . .          . . . . . . . . . . .           . . . . . . . . . . .",
+#endif
+
+#ifdef ANTI
     // "The king has no royal power and accordingly:
     // it may be captured like any other piece
     // there is no check or checkmate there is no castling"
     // -- https://en.wikipedia.org/wiki/Losing_chess
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1",
-    #endif
-    #ifdef ATOMIC
+#endif
+#ifdef ATOMIC
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef CRAZYHOUSE
+#endif
+#ifdef CRAZYHOUSE
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[] w KQkq - 0 1",
-    #endif
-    #ifdef EXTINCTION
+#endif
+#ifdef EXTINCTION
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef GRID
+#endif
+#ifdef GRID
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef HORDE
+#endif
+#ifdef HORDE
     "rnbqkbnr/pppppppp/8/1PP2PP1/PPPPPPPP/PPPPPPPP/PPPPPPPP/PPPPPPPP w kq - 0 1",
-    #endif
-    #ifdef KOTH
+#endif
+#ifdef KOTH
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef LOSERS
+#endif
+#ifdef LOSERS
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef RACE
+#endif
+#ifdef RACE
     "8/8/8/8/8/8/krbnNBRK/qrbnNBRQ w - - 0 1",
-    #endif
-    #ifdef THREECHECK
+#endif
+#ifdef THREECHECK
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 3+3 0 1",
-    #endif
-    #ifdef TWOKINGS
+#endif
+#ifdef TWOKINGS
     "rnbqkknr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKKNR w KQkq - 0 1",
-    #endif
-    #ifdef SUICIDE
+#endif
+#ifdef SUICIDE
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1",
-    #endif
-    #ifdef BUGHOUSE
+#endif
+#ifdef BUGHOUSE
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[] w KQkq - 0 1",
-    #endif
-    #ifdef DISPLACEDGRID
+#endif
+#ifdef DISPLACEDGRID
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef LOOP
+#endif
+#ifdef LOOP
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[] w KQkq - 0 1",
-    #endif
-    #ifdef PLACEMENT
+#endif
+#ifdef PLACEMENT
     "8/pppppppp/8/8/8/8/PPPPPPPP/8[KQRRBBNNkqrrbbnn] w - -",
-    #endif
-    #ifdef SLIPPEDGRID
+#endif
+#ifdef SLIPPEDGRID
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    #endif
-    #ifdef TWOKINGSSYMMETRIC
+#endif
+#ifdef TWOKINGSSYMMETRIC
     "rnbqkknr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKKNR w KQkq - 0 1",
-    #endif
-    #ifdef XIANGQI
+#endif
+#ifdef XIANGQI
     "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1",
-    #endif
-    #ifdef MODE_STRATEGO
+#endif
+#ifdef MODE_STRATEGO
     "MBCaaaaaaaKaaaaaaaaaaaaaaDaaaaaaEaDaaaLaaa__aa__aaaa__aa__aaPaaaWNaOXaQPaaaYaaaaaaaaaaaaaaaaaaaaaaaa r 0",
-    #endif
-       
+#endif
+
 };
 
 #endif // VARIANTS_H
