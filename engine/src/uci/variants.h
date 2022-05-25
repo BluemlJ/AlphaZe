b0171fac7845cf7b33c3c99a9c10d6ae4d1c50ee
Jannis Blüml <me@jblue.ml>
2022-05-25 15:05:28 +0200

    do not check variant

--- engine/src/uci/variants.h
+++ engine/src/uci/variants.h
@@ -73,11 +73,6 @@ const int SUBVARIANT_NB = 20; // Thats high quality code
 
 const static string StartFENs[SUBVARIANT_NB] = {
 
-#ifdef MODE_OPEN_SPIEL
-    //". . . . . . . . . . .  . . . . . . . . . . .   . . . . . . . . . . .    . . . . . . . . . . .     . . . . . . . . . . .      . . . . . . . . . . .       . . . . . . . . . . .        . . . . . . . . . . .         . . . . . . . . . . .          . . . . . . . . . . .           . . . . . . . . . . .",
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-#endif
-
 #ifdef ANTI
     // "The king has no royal power and accordingly:
     // it may be captured like any other piece
